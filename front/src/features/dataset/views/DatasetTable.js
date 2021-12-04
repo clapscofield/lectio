ea5407db7978ded8db60d3cd3f8563bf2ca9bbca
clapscofield <clarisse.scofield@gmail.com>
2021-12-04 17:28:25 -0300

    Mudanças para ler dados csv e inserir na tabela

--- front/src/features/dataset/views/DatasetTable.js
+++ front/src/features/dataset/views/DatasetTable.js
@@ -1,15 +1,279 @@
-import React, {useEffect} from "react";
-import Papa from "papaparse";
-import 
+import React, { useEffect, useState } from "react";
+import { readString } from "react-papaparse";
+import goodreadsWorks from "../../../assets/csv/goodreads_works.csv";
+import PropTypes from "prop-types";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import { visuallyHidden } from "@mui/utils";
+import Icon from '@mui/material/Icon';
+
 
 export default function DatasetTable() {
-    const [csvFile, setCsvFile] = useState(); 
+  function createData(name, calories, fat, carbs, protein) {
+    return {
+      name,
+      calories,
+      fat,
+      carbs,
+      protein
+    };
+  }
 
-    useEffect(() => {
-        setCsvFile(x);
-        console.log(csvFile);
+  const rows = [
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Donut", 452, 25.0, 51, 4.9),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("Honeycomb", 408, 3.2, 87, 6.5),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Jelly Bean", 375, 0.0, 94, 0.0),
+    createData("KitKat", 518, 26.0, 65, 7.0),
+    createData("Lollipop", 392, 0.2, 98, 0.0),
+    createData("Marshmallow", 318, 0, 81, 2.0),
+    createData("Nougat", 360, 19.0, 9, 37.0),
+    createData("Oreo", 437, 18.0, 63, 4.0)
+  ];
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator(order, orderBy) {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  // This method is created for cross-browser compatibility, if you don't
+  // need to support IE11, you can use Array.prototype.sort() directly
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
     });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  //id	title	isbn	isbn13	asin	image_url	publication_year	publication_month	publication_day	publisher	is_ebook	description	num_pages	format	format_summ	edition_information	average_rating	ratings_count	text_reviews_count	num_of_authors	similar_books	num_of_similar_books	popular_shelves	to_read	currently_reading	favorites	num_of_shelves	work_url
+  const headCells = [
+    {
+      id: "id",
+      numeric: true,
+      disablePadding: true,
+      label: "ID"
+    },
+    {
+      id: "titulo",
+      numeric: true,
+      disablePadding: false,
+      label: "Título"
+    },
+    {
+      id: "isbn",
+      numeric: true,
+      disablePadding: false,
+      label: "ISBN"
+    },
+    {
+      id: "isbn13",
+      numeric: true,
+      disablePadding: false,
+      label: "ISBN13"
+    },
+    {
+      id: "asin",
+      numeric: true,
+      disablePadding: false,
+      label: "ASIN"
+    },
+    {
+      id: "image",
+      numeric: false,
+      disablePadding: false,
+      label: "URL IMAGEM"
+    }
+  ];
 
+  function EnhancedTableHead(props) {
+    const {
+      order,
+      orderBy,
+      onRequestSort
+    } = props;
+
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? "right" : "left"}
+              padding={headCell.disablePadding ? "none" : "normal"}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : "asc"}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === "desc"
+                      ? "sorted descending"
+                      : "sorted ascending"}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+  EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+    orderBy: PropTypes.string.isRequired
+  };
+
+  const EnhancedTableToolbar = (props) => {
+    const { numSelected } = props;
+
+    return (
+      <Toolbar
+        sx={{
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+          ...(numSelected > 0 && {
+            bgcolor: (theme) =>
+              alpha(
+                theme.palette.primary.main,
+                theme.palette.action.activatedOpacity
+              )
+          })
+        }}
+      >
+          <Typography
+            sx={{ flex: "1 1 100%" }}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            Goodreads Works
+          </Typography>
+        
+
+        <Tooltip title="Filter list">
+          <IconButton>
+          <Icon baseClassName="fas" className="fa-regular fa-filter" />
+          </IconButton>
+        </Tooltip>
+      </Toolbar>
+    );
+  };
+
+  EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired
+  };
+
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("titulo");
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [dados, setDados] = useState([]);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const papaConfig = {
+    delimiter: "\t",
+    header: "true",
+    complete: (results, file) => {
+      setDados(results.data);
+      console.log("Dados: ", dados);
+    },
+    download: true,
+    error: (error, file) => {
+      console.log("Error while parsing:", error, file);
+    }
+  };
+  
+  readString(goodreadsWorks, papaConfig);
 
   return (
     <>
@@ -17,14 +281,95 @@ export default function DatasetTable() {
         <div className="w-full px-4">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
             <div className="flex flex-wrap w-full px-12 mr-auto ml-auto mt-6">
-              <div className="flex flex-wrap justify-center">
-                <div className="w-6/12 sm:w-4/12">
-                <h1 className="text-5xl mb-2 font-semibold leading-normal">
-                   Visualização tabela de dados 
-              </h1>
-                 
-                </div>
-              </div>
+                  <h1 className="text-5xl mb-2 font-semibold leading-normal">
+                    Visualização tabela de dados
+                  </h1>
+                  <Box sx={{ width: "100%" }}>
+                    <Paper sx={{ width: "100%", mb: 2 }}>
+                      <EnhancedTableToolbar numSelected={selected.length} />
+                      <TableContainer>
+                        <Table
+                          sx={{ minWidth: 750 }}
+                          aria-labelledby="tableTitle"
+                          size={dense ? "small" : "medium"}
+                        >
+                          <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                          />
+                          <TableBody>
+                            {stableSort(rows, getComparator(order, orderBy))
+                              .slice(
+                                page * rowsPerPage,
+                                page * rowsPerPage + rowsPerPage
+                              )
+                              .map((row, index) => {
+                                const labelId = `enhanced-table-checkbox-${index}`;
+
+                                return (
+                                  <TableRow
+                                    hover
+                                    onClick={(event) =>
+                                      handleClick(event, row.name)
+                                    }
+                                    role="checkbox"
+                                    tabIndex={-1}
+                                    key={row.name}
+                                  >
+                                    <TableCell
+                                      component="th"
+                                      id={labelId}
+                                      scope="row"
+                                      padding="none"
+                                    >
+                                      {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                      {row.calories}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                      {row.fat}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                      {row.carbs}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                      {row.protein}
+                                    </TableCell>
+                                  </TableRow>
+                                );
+                              })}
+                            {emptyRows > 0 && (
+                              <TableRow
+                                style={{
+                                  height: (dense ? 33 : 53) * emptyRows
+                                }}
+                              >
+                                <TableCell colSpan={6} />
+                              </TableRow>
+                            )}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                      <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                      />
+                    </Paper>
+                    <FormControlLabel
+                      control={
+                        <Switch checked={dense} onChange={handleChangeDense} />
+                      }
+                      label="Dense padding"
+                    />
+                  </Box>
+
             </div>
           </div>
         </div>
