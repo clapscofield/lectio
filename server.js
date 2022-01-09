bbdeb3925088f15bc9686f2966ca93ead65129d2
clapscofield <clarisse.scofield@gmail.com>
2022-01-09 13:21:48 -0300

    Mudanças nas pastas

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 3000);
  
 const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+  console.log('listening on port', server.address().port);
 });
